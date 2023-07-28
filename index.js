a76eaaccd5c1777d011519c619ebd6e9ced5feab
JoshLegend123 <ukakajoshua@gmail.com>
2023-07-28 23:57:51 +0100

    final commit

--- index.js
+++ index.js
@@ -8,52 +8,60 @@ menu.addEventListener('click', function () {
     })
 })
 let features = document.getElementById('features')
-    let arrowFeature = document.getElementById('arrow-feature')
-    let popupFeatures = document.getElementById('popup-features')
+let arrowFeature = document.getElementById('arrow-feature')
+let popupFeatures = document.getElementById('popup-features')
+let heading = document.getElementById('heading')
+let company = document.getElementById('company')
+const mediaQuery = window.matchMedia('(max-width: 499px)')
+features.onmouseenter = function () {
+    arrowFeature.src = 'images/icon-arrow-up.svg'
+    popupFeatures.style.display = 'initial'
+    if (mediaQuery.matches) {
+        popupFeatures.style.left = '120px'
+        popupFeatures.style.marginLeft   = '100px'
+        popupFeatures.style.top = '15%'
+        company.style.marginTop = '150px'
+        features.onmouseleave = function () {
+            arrowFeature.src = 'images/icon-arrow-down.svg'
+            popupFeatures.style.display = 'none'
+            company.style.marginTop = '0px'
+        }
+    } else {
+        heading.style.marginTop = '70px'
+        features.onmouseleave = function () {
+            arrowFeature.src = 'images/icon-arrow-down.svg'
+            popupFeatures.style.display = 'none'
+            heading.style.marginTop = '0px'
+        }
+    }
+}
+let arrowCompany = document.getElementById('arrow-company')
+let popupCompany = document.getElementById('popup-company')
+let careers = document.getElementById('careers')
+company.onmouseenter = function () {
+    arrowCompany.src = 'images/icon-arrow-up.svg'
+    popupCompany.style.display = 'flex'
+    if (mediaQuery.matches) {
+        popupCompany.style.left = '52%'
+        popupCompany.style.top = '19%'
+        careers.style.marginTop = '100px'
+        company.onmouseleave = function () {
+            arrowCompany.src = 'images/icon-arrow-down.svg'
+            popupCompany.style.display = 'none'
+            careers.style.marginTop = '10px'
+        }
+    } else {
+        company.onmouseleave = function () {
+            arrowCompany.src = 'images/icon-arrow-down.svg'
+            popupCompany.style.display = 'none'
+        }
+    }
+}
+if (mediaQuery.matches) {
+    let heroImg = document.getElementById('hero-img')
+    heroImg.src = 'images/image-hero-mobile.png'
     let heading = document.getElementById('heading')
-    let company = document.getElementById('company')
-    const mediaQuery = window.matchMedia('(max-width: 499px)')
-    features.onmouseenter = function () {
-        arrowFeature.src = 'images/icon-arrow-up.svg'
-        popupFeatures.style.display = 'initial'
-        if (mediaQuery.matches) {
-            popupFeatures.style.left = '120px'
-            popupFeatures.style.marginLeft   = '20px'
-            popupFeatures.style.top = '14%'
-            company.style.marginTop = '150px'
-            features.onmouseleave = function () {
-                arrowFeature.src = 'images/icon-arrow-down.svg'
-                popupFeatures.style.display = 'none'
-                company.style.marginTop = '0px'
-            }
-        } else {
-            heading.style.marginTop = '70px'
-            features.onmouseleave = function () {
-                arrowFeature.src = 'images/icon-arrow-down.svg'
-                popupFeatures.style.display = 'none'
-                heading.style.marginTop = '0px'
-            }
-        }
-    }
-    let arrowCompany = document.getElementById('arrow-company')
-    let popupCompany = document.getElementById('popup-company')
-    let careers = document.getElementById('careers')
-    company.onmouseenter = function () {
-        arrowCompany.src = 'images/icon-arrow-up.svg'
-        popupCompany.style.display = 'flex'
-        if (mediaQuery.matches) {
-            popupCompany.style.left = '48%'
-            popupCompany.style.top = '23%'
-            careers.style.marginTop = '100px'
-            company.onmouseleave = function () {
-                arrowCompany.src = 'images/icon-arrow-down.svg'
-                popupCompany.style.display = 'none'
-                careers.style.marginTop = '10px'
-            }
-        } else {
-            company.onmouseleave = function () {
-                arrowCompany.src = 'images/icon-arrow-down.svg'
-                popupCompany.style.display = 'none'
-            }
-        }
-    }
+    heading.innerHTML = '<h1>Make remote work</h1>'
+    let description = document.getElementById('description')
+    description.innerHTML = `<p>Get your team in sync, no matter your `<br>` location. Streamline processes, create `<br>` team rituals, and watch productivity soar.</p>`
+}
